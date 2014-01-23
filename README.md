f8da4f7b0b642fe44800cefa3004771a251c656e
Oliver Kraitschy <okraits@arcor.de>
2014-01-23 10:31:21 +0100

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,47 @@
+# README for omodoro
+
+## Contents:
+
+1. Overview
+2. Requirements
+3. Usage
+4. License
+5. Authorship, feedback, questions and contributions
+
+### 1. Overview
+
+omodoro is, as the name suggests, a tool for using the pomodoro technique.
+
+Currently it provides the following features:
+
+- display periodic reminders for pomodori and breaks
+
+### 2. Requirements
+
+omodoro has the following requirements:
+
+- Python Version 2 or 3
+- libnotify
+
+### 3. Usage
+
+- Customize the variables in the SETTINGS section to your needs
+- Run the script with python
+
+### 4. License
+
+This software is released under the terms of the
+GNU General Public License v2:
+
+[http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt](http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
+
+### 5. Authorship, feedback, questions and contributions
+
+There is a git repository available at github:
+
+[https://github.com/okraits/omodoro](https://github.com/okraits/omodoro)
+
+This software was initiated by Oliver Kraitschy (http://okraits.de).
+Please feel free to send him feedback and questions regarding
+bugreports, feature requests, improvements, etc. via mail at
+[okraits[at]arcor[dot]de](mailto:okraits@arcor.de).
---
+++ omodoro.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+# pomodoro script by Oliver Kraitschy
+# http://okraits.de okraits@arcor.de
+# https://github.com/okraits/omodoro
+#
+# TODO
+# -read length of pomodori and breaks from config file
+# -add possibilty to pause the process
+# -require acknowledgement for next pomodoro/break
+# -gtk GUI
+
+from os import system
+from datetime import datetime, timedelta
+from time import sleep
+
+VERSION = "0.1"
+
+# SETTINGS
+# adjust the pomodoro cycle to your needs
+num_pomodori = 4 # number of pomodori to do in a cycle
+length_pomodori = 25 # length of one pomodori in minutes
+length_short_break = 5 # length of a short break in minutes
+length_long_break = 15 # length of a long break in minutes
+
+
+# global variables
+class States:
+    Pomodori, ShortBreak, LongBreak = range(3)
+cnt_pomodori = num_pomodori # pomodori left in the current cycle
+cnt_short_breaks = num_pomodori - 1 # short breaks left in the current cycle
+end_time = None # end time of the current state
+state = States.Pomodori
+
+def changeState(newState, length):
+    global end_time, state
+    title = ""
+    description = ""
+    if newState == States.Pomodori:
+        title = "Next Pomodori"
+        description = "Please start with the next Pomodori!\n\nEnd Time:"
+    elif newState == States.ShortBreak:
+        title = "Short Break"
+        description = "Please take a short break!\n\nEnd Time:"
+    elif newState == States.LongBreak:
+        title = "Long Break"
+        description = "Please take a long break!\n\nEnd Time:"
+    else:
+        # Something went wrong - exit
+        exit(1)
+    end_time = datetime.now() + timedelta(minutes=length)
+    system("notify-send -u critical '%s' '%s %s'" % (title, description, end_time.strftime("%H:%M")))
+    state = newState
+
+def pomodoroLoop():
+    global cnt_pomodori, cnt_short_breaks, state
+
+    while True:
+        if state == States.Pomodori:
+            # pomodori is not over
+            if end_time > datetime.now():
+                pass
+            else:
+                # decrease number of pomodori for the current cycle
+                cnt_pomodori -= 1
+                if length_short_break > 0 and cnt_short_breaks > 0:
+                    # length of short breaks > 0 and short breaks left -> start short break
+                    changeState(States.ShortBreak, length_short_break)
+                elif length_short_break == 0 and cnt_pomodori != 0:
+                    # no short breaks -> start next pomodori
+                    changeState(States.Pomodori, length_pomodori)
+                elif cnt_pomodori == 0:
+                    # last pomodori over -> start long break
+                    changeState(States.LongBreak, length_long_break)
+                else:
+                    # Something went wrong - exit
+                    exit(1)
+        elif state == States.ShortBreak:
+            # short break is not over
+            if end_time > datetime.now():
+                pass
+            else:
+                # decrease number of short breaks for the current cycle
+                cnt_short_breaks -= 1
+                # start next pomodori
+                changeState(States.Pomodori, length_pomodori)
+            pass
+        elif state == States.LongBreak:
+            # long break is not over
+            if end_time > datetime.now():
+                pass
+            else:
+                # re-init variables, start next cycle
+                cnt_pomodori = num_pomodori
+                cnt_short_breaks = num_pomodori - 1
+                changeState(States.Pomodori, length_pomodori)
+        else:
+            # Something went wrong - exit
+            exit(1)
+        sleep(30)
+        
+
+if __name__ == "__main__":
+
+    # start first pomodori
+    changeState(States.Pomodori, length_pomodori)
+    pomodoroLoop()
+    exit(0)
